110a9a734dde9f60663e69993a8e491ab5ac78b0
Maxime Gismero <maxime.gismero@gmail.com>
2024-08-20 17:06:36 +0200

    init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen text-center pt-64">
+      <h1 className="text-8xl">
+        <span className="text-violet-500">be</span>formatic
+      </h1>
+      <h2 className="text-2xl">business engineering in IT</h2>
+    </main>
+  );
+}
